64f61d183a7deabfa70292400e582326450f430b
Vlad Nicula <vladnicula@gmail.com>
2022-08-25 17:41:12 +0300

    Move image at the top

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <Image src="/temp.jpeg" alt="Vercel Logo" width={536 } height={ 354} />
+
 
         <Link href='/persons/1'>Person 1</Link>
 
@@ -55,19 +59,7 @@ export default function Home() {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/temp.jpeg" alt="Vercel Logo" width={536 } height={ 354} />
-          </span>
-        </a>
-      </footer>
+      
     </div>
   )
 }
